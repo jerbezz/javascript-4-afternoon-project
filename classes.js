541f3b99695b3c99607faba3d4ae9f8074a8ae06
Jeremy Bezzant <jeremybezzant@Jeremys-MacBook-Pro.local>
2019-03-08 16:43:33 -0700

    finshed classes and context

--- classes.js
+++ classes.js
@@ -1,7 +1,7 @@
 /* 
   Once you complete a problem, refresh ./classes.html in your browser and check to see if the problem's test(s) are passing.
   Passed tests will be indicated by a green circle.
-  Failed tests will be indicated by a red X.
+  Failed tests will be indicated by a red X. 
 
   You can refresh the page at any time to re-run all the tests.
 
@@ -30,6 +30,18 @@
 */
 
 //Code Here
+class Employee{
+  constructor (first, last, email, age){
+    this.first_name = first;
+    this.last_name = last;
+    this.email = email;
+    this.age = age;
+  }
+
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -48,7 +60,20 @@
 */
 
 //Code Here
+class Manager extends Employee{
+  constructor (first, last, email, age){
+    super(first, last, email, age) 
+      this.reports = []; 
+  }
+  hire(employee){
+    this.reports.push(employee)
+  }
 
+  fire(i){
+    this.reports.splice(i, 1)
+  }
+
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -72,7 +97,36 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager{
+  constructor(first, last, email, age, reports){
+  super(first, last, email, age, reports)
+  this.title = "Not a manager";
+  this.bonus = 0;
+  }
 
+  hire(employee){
+    this.reports.push(employee)
+  if(this.reports.length >=1 && this.reports.length <=3){
+      this.title = "Barely Manager"
+    } else if(this.reports.length >=4 && this.reports.length <=10){
+      this.title = "Mostly Manager"
+    }else if (this.reports.length >=11 && this.reports.length <=50){
+      this.title = "Manager"
+    }else if (this.reports.length >=51 && this.reports.length <=100){
+      this.title = "Manager Plus"
+    }else if (this.reports.length >100){
+      this.title = "Bestest Manager"
+    }
+    return this.title
+
+  }
+
+  fire(i){
+    this.reports.splice(i, 1)
+    this.bonus += 100
+  }
+  
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -99,5 +153,37 @@
 */
 
 //Code Here
+class Machine{
+  constructor (){
+    this.widgets_made_count = 0;
+    this.wear_and_tear_count = 0;
+    this.needs_reboot = false;
+  }
 
+  makeWidgets(num){
+    this.widgets_made_count += num;
+    this.wear_and_tear_count += (num/50)
+  }
+
+  fixMachine(){
+    this.needs_reboot = true;
+  }
+
+  reboot(){
+    return function() {
+     
+        
+        this.wear_and_tear_count -= 10
+        this.needs_reboot = false
+      }
+      .bind(this)
+}
+  
+  
+
+
+
+
+
+}
 
--- context.js
+++ context.js
@@ -20,7 +20,13 @@
   
 
 //Code Here
-
+let user = {
+  username: 'Jweezy05',
+  email: 'Jweezy05@email.com',
+  getUsername: function(){
+    return this.username
+  }
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -42,7 +48,7 @@ class Animal {
 }
 
 //Code Here
-
+let animal1 = new Animal('Ruffus', "dog", "kibble")
 
 
 ////////// PROBLEM 3 //////////
@@ -63,6 +69,7 @@ let who = {
 
 //Code Here
 
+let whoSaysHi = sayHi.bind(who)
 
 
 ////////// PROBLEM 4 //////////
@@ -78,7 +85,7 @@ function whatIsThis() {
 //Code Here
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = window
 
 let product = {
   name: 'snake plant',
@@ -89,7 +96,7 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+let context2 = product;
 
 let vacation = {
   location: 'Hawaii',
@@ -100,7 +107,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation
 
 
 class Family {
@@ -118,4 +125,4 @@ class Family {
 let family1 = new Family(2, 4, 1)
 
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-//let context4 = ???
+let context4 = family1
